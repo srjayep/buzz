62ddfa87a7660cf6316d1fc0caf0a9d330a60b01
George McIntosh <george.mcintosh@specsavers.com>
2013-04-19 11:44:06 +0100

    Can now look up and delete servers by regex on hostname

--- lib/system.rb
+++ lib/system.rb
@@ -32,6 +32,50 @@ class System < Sif::Loader
    
   end
 
+  desc "List systems", "List all subscribed systems"
+  def list
+     server = XMLRPC::Client.new(@spacewalk_server, "/rpc/api", 80)
+      begin
+        key = server.call("auth.login", @username, @password)
+        puts "Using session key #{key}"
+        response = server.call("system.listSystems", 
+              key
+            )
+        systems = []
+        response.each do |system|
+          id = system['id']
+          name = system['name']
+          last_checkin = system['last_checkin'].to_time
+          puts "System: ID: #{id} name: #{name} last checked in: #{last_checkin}"
+        end
+      rescue XMLRPC::FaultException => e
+        puts "Error:"
+        puts e.faultCode
+        puts e.faultString
+      end 
+    end
+
+    desc "List by regex", "list"
+    def list_by_regex(regex)
+      
+        systems = find_systems_by_regex(regex)
+        systems.each do |system|
+          id = system['id']
+          name = system['name']
+          last_checkin = system['last_checkin'].to_time
+          puts "System: ID: #{id} name: #{name} last checked in: #{last_checkin}"
+        end
+      
+    end
+
+    desc "Delete by regex", "Delete all systems whose name matches the regex supplied"
+    def delete_by_regex(regex)
+      systems_to_delete = find_systems_by_regex regex
+      system_ids = systems_to_delete.collect { |system| system['id']}
+      system_ids.each { |it| puts "Deleting #{it}"}
+      delete_systems system_ids
+    end
+
   no_tasks do 
 
     def delete_systems(system_ids) 
@@ -50,6 +94,25 @@ class System < Sif::Loader
         puts e.faultCode
         puts e.faultString
       end 
+    end
+
+    def find_systems_by_regex(regex)
+      
+      server = XMLRPC::Client.new(@spacewalk_server, "/rpc/api", 80)
+      systems = []
+        begin
+          key = server.call("auth.login", @username, @password)
+          puts "Using session key #{key}"
+          systems = server.call("system.searchByName", 
+              key,
+              regex
+           )
+        rescue XMLRPC::FaultException => e
+          puts "Error:"
+          puts e.faultCode
+          puts e.faultString 
+        end
+       systems
     end
 
   end
