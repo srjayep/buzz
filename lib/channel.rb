ebe1c29cc51ef2d019e329140deac0d9b26f8af5
George McIntosh <george.mcintosh@specsavers.com>
2013-04-19 15:46:47 +0100

    Can now regenerate yum cache on a channel

--- lib/channel.rb
+++ lib/channel.rb
@@ -5,6 +5,26 @@ require "xmlrpc/client"
 
 class Channel < Sif::Loader
 
+  desc "Regenerate YUM cache", "This call allows you to force the re-generation of the YUM cache for the specified channel"
+  def refresh(channel_label)
+      server = XMLRPC::Client.new(@spacewalk_server, "/rpc/api", 80)
+      puts "Regenerating YUM cache for #{channel_label}"
+     
+      begin
+        key = server.call("auth.login", @username, @password)
+        puts "Using session key #{key}"
+        out = server.call("channel.software.regenerateYumCache", 
+              key,
+              channel_label
+              )
+        puts out
+      rescue XMLRPC::FaultException => e
+        puts "Error:"
+        puts e.faultCode
+        puts e.faultString
+      end
+  end 
+
   option :name, :aliases => ['-n']
   option :summary, :aliases => ['-s']
   option :arch, :aliases => ['-a']
@@ -12,7 +32,7 @@ class Channel < Sif::Loader
   option :checksum, :default => 'sha256'
   desc "Create channel", "Creates a new channel"
   def create(channel_label)
-    server = XMLRPC::Client.new("spacewalk.elevenware.com", "/rpc/api", 80)
+    server = XMLRPC::Client.new(@spacewalk_server, "/rpc/api", 80)
     puts "Creating #{channel_label}"
     puts "Name #{options[:name]}"
     puts "Summary #{options[:summary]}"
@@ -26,7 +46,7 @@ class Channel < Sif::Loader
     checksum = options[:checksum]
 
     begin
-      key = server.call("auth.login", "admin", "admin")
+      key = server.call("auth.login", @username, @password)
       puts "Using session key #{key}"
       out = server.call("channel.software.create", 
             key,
